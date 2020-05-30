9ccd36967cb5ebead0676a59455d41cb5a9cf7b0
Michał Krzysztof Feiler <m@mikf.pl>
2020-05-31 00:31:37 +0200

    started working on class TableFile

--- zad.py
+++ zad.py
@@ -4,6 +4,61 @@ from pyramid.response import Response
 from tinydb import TinyDB, Query, where
 from jsonschema import validate
 
+class TableFile:
+
+    class WrongFieldSize(Exception):
+        pass
+
+    class WrongSetOfFields(Exception):
+        pass
+
+    _LAYOUT_SCHEMA = {
+        "type": "object",
+        "patternProperties": { "^[a-z]{3,15}$": {"type": "integer", "exclusiveMinimum": 0} },
+        "additionalProperties": false 
+    }
+    
+    def __init__(self, filename : str, layout : dict):
+        validate(layout, _LAYOUT_SCHEMA)
+        self._filename : str = filename
+        self._fileopen = open(filename, "a+b")
+        self._layout = layout
+        offsets = {}
+        order = []
+        acc = 0
+        for k, v in layout.items():
+            order.append((k, v, acc, acc+v))
+            offsets[k] = (acc, acc + v)
+            acc += v
+        self._offsets = offsets
+        self._order = order
+        self._rowsize = acc
+
+    @property
+    def row_size(self) -> int:
+        return self._row_size
+
+    @property
+    def layout(self) -> dict:
+        return self._layout.copy()
+
+    @property
+    def filename(self) -> str:
+        return self._filename
+
+    def _field(self, b, n):
+        assert type(b) is bytes
+        if len(b)!=n:
+            raise WrongFieldSize()
+        self._fileopen.write(b)
+
+    def add(self, **r):
+        if set(r.keys())!=set(self._layout.keys()):
+            raise WrongSetOfFields()
+        for k, v, _, _ in self._order:
+            _field(r[k], v)
+
+
 db = TinyDB('db.json')
 table = db.table('recipes')
 
