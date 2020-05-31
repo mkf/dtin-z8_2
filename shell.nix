c4566ba3608c19a0de6b4a6698e39a80c827853c
Michał Krzysztof Feiler <m@mikf.pl>
2020-05-31 17:54:41 +0200

    removed tinydb and python-language-server from shell.nix

--- shell.nix
+++ shell.nix
@@ -5,8 +5,6 @@ pkgs.mkShell {
   buildInputs = with pkgs; [
     (python3.withPackages (python-packages: with python-packages; [
         pyramid_jinja2 jinja2 pyramid
-        tinydb
-        python-language-server
         pylint autopep8
       ]))
   ];
